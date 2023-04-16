c7813aec7af00ba74b1610e20af4fb88b894ee84
Lau22 <lauracrisanchez22@gmail.com>
2023-04-16 12:55:00 +0200

    add branch Lau-dev

--- src/components/common/Signin.jsx
+++ src/components/common/Signin.jsx
@@ -1,37 +1,46 @@
 import React, { useEffect, useState } from "react";
+import styles from "../../styles/css/main.css";
 
 
 function Signin() {
   const [repo, setRepo] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-      fetch('http://127.0.0.1:8000/apisignin/list')
+    fetch('http://127.0.0.1:8000/apisignin/list')
       .then(response => response.json())
-  
-      .then(repo => 
-          setRepo(repo) )// Establece el valor de 'repo' primero
-           // Puedes dejar esto aquí si quieres, pero no es necesari)
+      .then(repo => {
+        setRepo(repo);
+        setLoading(false);
+      })
       .catch(error => console.error(error));
-      
   }, []);
-    
-    
 
   return (
-    <div>
-        {repo.map(int => {
-                return(
-                    <div key={int.id}>
-                        <h3>{int.timestart}</h3>
-                        <p>{int.timestop}</p> 
-                        <p>{int.timerestart}</p> 
-                        <p>{int.timefinish}</p>
-                        <p>{int.hourcount}</p>
-                    </div>
-                )
-            })} 
+    <div className={styles.container} style={{ backgroundColor: "rgba(217, 217, 217, 1)" }}>
+      <h1 className={styles.title}>Signin Data</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {repo.map(int => (
+            <div key={int.id} className={styles.item}>
+              <h3 className={styles.subtitle}>Time Start: {int.timestart}</h3>
+              <p>Time Stop: {int.timestop}</p> 
+              <p>Time Restart: {int.timerestart}</p> 
+              <p>Time Finish: {int.timefinish}</p>
+              <p>Hour Count: {int.hourcount}</p>
+              <div className={styles.actions}>
+                <button className={styles.button} style={{ backgroundColor: "rgba(0, 136, 148, 1)", color: "#fff" }}>Fichar Entrada</button>
+                <button className={styles.button} style={{ backgroundColor: "rgba(0, 136, 148, 1)", color: "#fff" }}>Fichar Salida</button>
+              </div>
+            </div>
+          ))}
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default Signin
+export default Signin;
+
