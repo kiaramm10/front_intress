86fbfa11b2931fdbba453e1bba750ee0dc7a76db
22Lau <118261194+22Lau@users.noreply.github.com>
2023-04-17 10:39:56 +0200

    create Holidays & Signin

--- src/components/common/Signin.jsx
+++ src/components/common/Signin.jsx
@@ -1,37 +1,115 @@
-import React, { useEffect, useState } from "react";
-
+import React, { useState, useEffect } from "react";
+// import styles from "../../styles/css/main.css";
 
 function Signin() {
-  const [repo, setRepo] = useState([]);
+  const [timestart, setTimestart] = useState("");
+  const [timestop, setTimestop] = useState("");
+  const [timefinish, setTimefinish] = useState("");
+  const [hourcount, setHourcount] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [lastDay, setLastDay] = useState("");
+  const [totalHours, setTotalHours] = useState(0);
 
-  useEffect(() => {
-      fetch('http://127.0.0.1:8000/apisignin/list')
-      .then(response => response.json())
-  
-      .then(repo => 
-          setRepo(repo) )// Establece el valor de 'repo' primero
-           // Puedes dejar esto aquí si quieres, pero no es necesari)
-      .catch(error => console.error(error));
+
+  const handleTimeStart = () => {
+    const now = new Date();
+    const day = now.toLocaleDateString("es-ES");
+    const time = now.toLocaleTimeString("es-ES");
+    setTimestart(`${day} ${time}`);
+    setIsRunning(true);
+  };
+
+  const handleTimeStop = () => {
+    const now = new Date();
+    const day = now.toLocaleDateString("es-ES");
+    const time = now.toLocaleTimeString("es-ES");
+    setTimestop(`${day} ${time}`);
+    setIsRunning(false);
+  };
+
+  const handleTimeRestart = () => {
+    const now = new Date();
+    const day = now.toLocaleDateString("es-ES");
+    const time = now.toLocaleTimeString("es-ES");
+    setTimestart(`${day} ${time}`);
+    setIsRunning(true);
+  };
+
+  const handleTimeFinish = () => {
+    const now = new Date();
+    const day = now.toLocaleDateString("es-ES");
+    const time = now.toLocaleTimeString("es-ES");
+    setTimefinish(`${day} ${time}`);
+    setIsRunning(false);
+
+    // Save the time worked for the last day
+    if (lastDay !== "") {
+      localStorage.setItem(lastDay, JSON.stringify({ start: timestart, stop: timestop }));
+    }
+
+    // Set the last day to the current day
+    setLastDay(day);
+  };
+
+  const calculateTotalHoursWorked = () => {
+        let total = 0;
+        for (let i = 0; i < localStorage.length; i++) {
+          const key = localStorage.key(i);
+          const { start, stop } = JSON.parse(localStorage.getItem(key));
+          const startTime = new Date(start);
+          const endTime = new Date(stop);
+          const timeDiff = endTime.getTime() - startTime.getTime();
+          const hoursWorked = Math.abs(timeDiff / (1000 * 60 * 60));
+          total += hoursWorked;
+        }
+        return total;
+      };
       
-  }, []);
-    
-    
+
+      useEffect(() => {
+        setTotalHours(calculateTotalHoursWorked());
+      }, [hourcount]);
+      
+
+  const formatTime = (timeInSeconds) => {
+    const hours = Math.floor(timeInSeconds / 3600);
+    return hours;
+  };
 
   return (
-    <div>
-        {repo.map(int => {
-                return(
-                    <div key={int.id}>
-                        <h3>{int.timestart}</h3>
-                        <p>{int.timestop}</p> 
-                        <p>{int.timerestart}</p> 
-                        <p>{int.timefinish}</p>
-                        <p>{int.hourcount}</p>
-                    </div>
-                )
-            })} 
+    <div className={styles.container} style={{ backgroundColor: "rgba(217, 217, 217, 1)" }}>
+      <h1 className={styles.title}>Fichar</h1>
+      <div className={styles.item}>
+        <label htmlFor="timestart">Hora de inicio</label>
+        <button onClick={handleTimeStart} className={styles.button}>
+          {timestart ? `Inicio (${timestart})` : "Inicio"}
+        </button>
+        <div className={styles.timeContainer}>{timestart}</div>
+      </div>
+      <div className={styles.item}>
+        <label htmlFor="timerestart">Control de jornada</label>
+        <button onClick={isRunning ? handleTimeStop : handleTimeRestart} className={styles.button}>
+          {isRunning ? "Stop" : "Restart"}
+        </button>
+      </div>
+      <div className={styles.item}>
+        <label htmlFor="timefinish">Hora de finalización</label>
+        <button onClick={handleTimeFinish} className={styles.button}>
+          {timefinish ? `Fin jornada (${timefinish})` : "Fin jornada"}
+        </button>
+      </div>
+      <div className={styles.item}>
+        <label htmlFor="hourcount">Horas trabajadas</label>
+        <input
+          type="text"
+          name="hourcount"
+          id="hourcount"
+          value={totalHours ? formatTime(totalHours) : 0}
+          readOnly
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Signin
+export default Signin;
